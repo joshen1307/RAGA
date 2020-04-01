ca1a7ef9138acb70c5999e38dbbae776817d4ee1
David Bastien <bastien@lofar6.jb.man.ac.uk>
2020-04-01 15:51:26 +0100

    March Update 2

--- PYTHON/.ipynb_checkpoints/genetic_algo-checkpoint.py
+++ PYTHON/.ipynb_checkpoints/genetic_algo-checkpoint.py
@@ -1,19 +1,32 @@
 from bga import BGA
 import numpy as np
+
+
+
+
+
+
 def values(arr):
-    print(*arr)
-    w = np.array([71, 34, 82, 23, 1, 88, 12, 57, 10, 68, 5, 33, 37, 69, 98, 24, 26, 83, 16, 26, 18, 43, 52, 71, 22, 65, 68, 8, 40, 40, 24, 72, 16, 34, 10, 19, 28, 13, 34, 98, 29, 31, 79, 33, 60, 74, 44, 56, 54, 17, 63, 83, 100, 54, 10, 5, 79, 42, 65, 93, 52, 64, 85, 68, 54, 62, 29, 40, 35, 90, 47, 77, 87, 75, 39, 18, 38, 25, 61, 13, 36, 53, 46, 28, 44, 34, 39, 69, 42, 97, 34, 83, 8, 74, 38, 74, 22, 40, 7, 94])
-    v = np.array([26, 59, 30, 19, 66, 85, 94, 8, 3, 44, 5, 1, 41, 82, 76, 1, 12, 81, 73, 32, 74, 54, 62, 41, 19, 10, 65, 53, 56, 53, 70, 66, 58, 22, 72, 33, 96, 88, 68, 45, 44, 61, 78, 78, 6, 66, 11, 59, 83, 48, 52, 7, 51, 37, 89, 72, 23, 52, 55, 44, 57, 45, 11, 90, 31, 38, 48, 75, 56, 64, 73, 66, 35, 50, 16, 51, 33, 58, 85, 77, 71, 87, 69, 52, 10, 13, 39, 75, 38, 13, 90, 35, 83, 93, 61, 62, 95, 73, 26, 85])
-    w_ = np.sum(w * arr)
-    if w_ > 1000:
-#         print(np.sum(w * arr))
-        return 1.0 / (w_ - 1000)
-    else:
-        return np.sum(v * arr)
+    num_1 = int("".join(str(i) for i in arr[0:5]),2)
+    num_2 = int("".join(str(i) for i in arr[5:10]),2)
+    num_3 = int("".join(str(i) for i in arr[10:15]),2)
+    num_4 = int("".join(str(i) for i in arr[15:20]),2)
+    
+    sol_1 = num_1 + num_2
+    sol_2 = num_4 - num_3
+    sol_3 = num_2+num_3
+    sol_4 = num_1+num_4
+    
+    final_sol = (((8 - sol_1)**2.0)+((6-sol_2)**2)+((8-sol_3)**2)+((13-sol_4)**2))**0.5
+    print(final_sol)
+    
+    return final_sol
         
-num_pop = 3
 
-problem_dimentions = 100
+        
+num_pop = 1000
 
-test = BGA(pop_shape=(num_pop, problem_dimentions), method=values, p_c=0.8, p_m=0.2, max_round = 20, early_stop_rounds=None, verbose = None, maximum=True)
+problem_dimentions = 20
+
+test = BGA(pop_shape=(num_pop, problem_dimentions), method=values, p_c=0.8, p_m=0.8, max_round = 1000000, early_stop_rounds=None, verbose = None, maximum=False)
 best_solution, best_fitness = test.run()
