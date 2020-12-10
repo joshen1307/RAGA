9304af526d27c1368bf86b35571362dd830c21df
joshen1307 <joshen0712@gmail.com>
2020-12-10 17:04:38 +0000

    Update 10

--- PYTHON/utils/classifier_fr_2.py
+++ PYTHON/utils/classifier_fr_2.py
@@ -2,14 +2,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision import datasets, transforms
-from torchsummary import summary
 from torch.utils.data.sampler import SubsetRandomSampler
 
 import numpy as np
 from tqdm import tqdm
 
 from models import HMT
-from plots import *
+#from plots import *
 from FRDEEP import FRDEEPN, FRDEEPF
 
 def classification_procedure():
